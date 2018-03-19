5ed49dbb701f49b6e4b985087042a0652f1b8872
wansboods <wansboods@gmail.com>
2018-03-19 18:34:39 +0800

    Initial commit

---
+++ src/main/java/com/bevixtech/hdvod/Controller/MainController.java
@@ -0,0 +1,12 @@
+package com.bevixtech.hdvod.Controller;
+
+import com.bevixtech.hdvod.Model.User;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+    User user;
+
+
+}
